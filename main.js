3607329d5c894af3fb58f79950b9076b16d4039d
shaimaa <shisko939@gmail.com>
2024-10-31 21:58:09 +0200

    WIP

---
+++ main.js
@@ -0,0 +1,56 @@
+// Setting Game Name
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector(
+  "footer"
+).innerHTML = `${gameName} &copy; ${new Date().getFullYear()}  "Game Created By Elzero Web School"`;
+
+// Setting Game Options
+let numbersOfTries = 6;
+let numbersOfLetters = 6;
+let currentTry = 1;
+
+function generateInput() {
+  const inputsContainer = document.querySelector(".inputs");
+  // Create Main Try Div
+  for (let i = 1; i <= numbersOfTries; i++) {
+    const tryDiv = document.createElement("div");
+    tryDiv.classList.add(`try-${i}`);
+    tryDiv.innerHTML = `<span>Try ${i}</span>`;
+
+    if (i !== 1) tryDiv.classList.add("disabled-inputs");
+    // Create Inputes
+    for (let j = 1; j <= numbersOfLetters; j++) {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = `guess-${i}-letter-${i}`;
+      input.setAttribute("maxlength", "1");
+      tryDiv.appendChild(input);
+    }
+
+    inputsContainer.appendChild(tryDiv);
+  }
+  // Foucs On First Input In First Try Element
+  inputsContainer.children[0].children[1].focus();
+
+  // Disable All Inputs Except First One
+  const inputsInDisabledDiv = document.querySelectorAll(
+    ".disabled-inputs input"
+  );
+  inputsInDisabledDiv.forEach((input) => (input.disabled = true));
+
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input, index) => {
+    // conver Input to Uppercase
+    input.addEventListener("input", function () {
+      this.value = this.value.toUpperCase();
+      const nextInput = inputs[index + 1];
+      if (nextInput) nextInput.focus();
+    });
+  });
+}
+
+window.onload = function () {
+  generateInput();
+};
